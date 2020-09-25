d055639c8b34e0d54a6c348e16946a71f870cc0e
ahmedhosny0 <63015510+ahmedhosny0@users.noreply.github.com>
2020-09-25 13:46:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <body>  
     <div id= "frm">  
         <h1>Login</h1>  
-        <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">  
+        <form name="f1" action = "https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/login/authentication.php" onsubmit = "return validation()" method = "POST">  
             <ul id="userandpass">
                   <li id="Input"style="border-bottom:none;">
                    <label>UserName: </label><input type = "text"  name  = "user" id="user" > 

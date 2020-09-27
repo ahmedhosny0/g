dd74b9f374bad44ab3bb645f620ef4d468f830f2
ahmedhosny0 <63015510+ahmedhosny0@users.noreply.github.com>
2020-09-27 03:32:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <html>  
 <head>  
     <title>تسجيل الدخول للموقع</title>   
-    <link rel = "stylesheet" type = "text/css" href = "https://raw.githack.com/ahmedhosny0/TheProgrammer.com/master/css/style.css">   
 </head>  
 <body>  
     <h1>Go to head</h1>
